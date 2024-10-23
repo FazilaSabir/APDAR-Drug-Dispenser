51f6888eb0d5f98dadca72e38ac4af2c21cb1e70
Fazila Patel <66115918+FazilaSabir@users.noreply.github.com>
2024-10-23 10:55:46 +0400

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using DrugDispenser.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DrugDispenser.Startup))]
+namespace DrugDispenser
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+    }
+}
